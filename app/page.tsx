03208fc88cd703f660f0cc120e5bf5eb188ff04e
Elwyn <elwynynion2@gmail.com>
2025-04-10 00:50:38 +0800

    //

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
-import { Seamless } from '@/components/Seamless';
-import { SecuritySection } from '@/components/SecuritySection';
-import { WalletSection } from '@/components/WalletSection';
-import { CommunitySection } from '@/components/CommunitySection';
-import { DeveloperSection } from '@/components/DeveloperSection';
-import { Footer } from '@/components/Footer';
-import { CryptoTable } from '../components/CryptoTable';
-import { HeroSection } from '../components/HeroSection';
-import { StatsSection } from '../components/StatsSection';
-import { PerpsSection } from '../components/PerpsSection';
-import { APYSection } from '@/components/APYSection';
+import { CommunitySection } from "@/components/CommunitySection";
+import { DeveloperSection } from "@/components/DeveloperSection";
+import { Footer } from "@/components/Footer";
+import { Seamless } from "@/components/Seamless";
+import { SecuritySection } from "@/components/SecuritySection";
+import { CryptoTable } from "../components/CryptoTable";
+import { HeroSection } from "../components/HeroSection";
+import { StatsSection } from "../components/StatsSection";
 
 export default function Page() {
 	return (
@@ -17,7 +14,7 @@ export default function Page() {
 
 			<StatsSection />
 
-			<div className="flex mx-4 my-6 main-container flex-col -space-y-16">
+			<div className="main-container mx-4 my-6 flex flex-col -space-y-16">
 				<CryptoTable />
 				<Seamless />
 				<SecuritySection />
